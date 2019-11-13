91c53e4b7b585a59f9cf979869ed69f3b6eb3867
sean-sn <sean@supranational.net>
2019-11-13 10:20:13 -0500

    Added twisted_edwards_jubjub_add into pedersen_hash_with_precomp

--- benches/pedersen_hash.rs
+++ benches/pedersen_hash.rs
@@ -8,7 +8,7 @@ extern crate test;
 use paired::bls12_381::Bls12;
 use rand::{thread_rng, Rand};
 use fil_sapling_crypto::jubjub::JubjubBls12;
-use fil_sapling_crypto::pedersen_hash::{pedersen_hash, Personalization};
+use fil_sapling_crypto::pedersen_hash::{pedersen_hash, pedersen_hash_with_precomp, Personalization};
 
 fn bench_pedersen_hash_aux(b: &mut test::Bencher, params: JubjubBls12) {
     let rng = &mut thread_rng();
@@ -16,6 +16,14 @@ fn bench_pedersen_hash_aux(b: &mut test::Bencher, params: JubjubBls12) {
     let personalization = Personalization::MerkleTree(31);
 
     b.iter(|| pedersen_hash::<Bls12, _>(personalization, bits.clone(), &params));
+}
+
+fn bench_pedersen_hash_precomp_aux(b: &mut test::Bencher, params: JubjubBls12) {
+    let rng = &mut thread_rng();
+    let bits = (0..510).map(|_| bool::rand(rng)).collect::<Vec<_>>();
+    let personalization = Personalization::MerkleTree(31);
+
+    b.iter(|| pedersen_hash_with_precomp::<Bls12, _>(personalization, bits.clone(), &params));
 }
 
 #[bench]
@@ -46,4 +54,34 @@ fn bench_pedersen_hash_18(b: &mut test::Bencher) {
 fn bench_pedersen_hash_19(b: &mut test::Bencher) {
     let params = JubjubBls12::new_with_window_size(19);
     bench_pedersen_hash_aux(b, params);
+}
+
+#[bench]
+fn bench_pedersen_hash_using_precomp(b: &mut test::Bencher) {
+    let params = JubjubBls12::new();
+    bench_pedersen_hash_precomp_aux(b, params);
+}
+
+#[bench]
+fn bench_pedersen_hash_using_precomp_16(b: &mut test::Bencher) {
+    let params = JubjubBls12::new_with_window_size(16);
+    bench_pedersen_hash_precomp_aux(b, params);
+}
+
+#[bench]
+fn bench_pedersen_hash_using_precomp_17(b: &mut test::Bencher) {
+    let params = JubjubBls12::new_with_window_size(17);
+    bench_pedersen_hash_precomp_aux(b, params);
+}
+
+#[bench]
+fn bench_pedersen_hash_using_precomp_18(b: &mut test::Bencher) {
+    let params = JubjubBls12::new_with_window_size(18);
+    bench_pedersen_hash_precomp_aux(b, params);
+}
+
+#[bench]
+fn bench_pedersen_hash_using_precomp_19(b: &mut test::Bencher) {
+    let params = JubjubBls12::new_with_window_size(19);
+    bench_pedersen_hash_precomp_aux(b, params);
 }
--- src/pedersen_hash.rs
+++ src/pedersen_hash.rs
@@ -1,6 +1,9 @@
 use ff::{Field, PrimeField, PrimeFieldRepr};
 use jubjub::*;
 
+use std::slice;
+use std::convert::TryInto;
+
 #[derive(Copy, Clone)]
 pub enum Personalization {
     NoteCommitment,
@@ -97,6 +100,106 @@ where
             let i = (acc.as_ref()[0] & window_mask) as usize;
 
             tmp = tmp.add(&table[0][i], params);
+
+            acc.shr(window);
+            table = &table[1..];
+        }
+
+        result = result.add(&tmp, params);
+    }
+
+    result
+}
+
+pub fn pedersen_hash_with_precomp<E, I>(
+    personalization: Personalization,
+    bits: I,
+    params: &E::Params,
+) -> edwards::Point<E, PrimeOrder>
+where
+    I: IntoIterator<Item = bool>,
+    E: JubjubEngine,
+{
+    let mut bits = personalization
+        .get_bits()
+        .into_iter()
+        .chain(bits.into_iter());
+
+    let mut result = edwards::Point::zero();
+    let mut generators = params.pedersen_hash_exp_table_precomp().iter();
+
+    loop {
+        let mut acc = E::Fs::zero();
+        let mut cur = E::Fs::one();
+        let mut chunks_remaining = params.pedersen_hash_chunks_per_generator();
+        let mut encountered_bits = false;
+
+        // Grab three bits from the input
+        while let Some(a) = bits.next() {
+            encountered_bits = true;
+
+            let b = bits.next().unwrap_or(false);
+            let c = bits.next().unwrap_or(false);
+
+            // Start computing this portion of the scalar
+            let mut tmp = cur;
+            if a {
+                tmp.add_assign(&cur);
+            }
+            cur.double(); // 2^1 * cur
+            if b {
+                tmp.add_assign(&cur);
+            }
+
+            // conditionally negate
+            if c {
+                tmp.negate();
+            }
+
+            acc.add_assign(&tmp);
+
+            chunks_remaining -= 1;
+
+            if chunks_remaining == 0 {
+                break;
+            } else {
+                cur.double(); // 2^2 * cur
+                cur.double(); // 2^3 * cur
+                cur.double(); // 2^4 * cur
+            }
+        }
+
+        if !encountered_bits {
+            break;
+        }
+
+        let mut table: &[Vec<edwards::Point<E, _>>] =
+            &generators.next().expect("we don't have enough generators");
+        let window = params.pedersen_hash_exp_window_size();
+        let window_mask = (1 << window) - 1;
+
+        let mut acc = acc.into_repr();
+
+        let mut tmp = edwards::Point::zero();
+        let mut tmp_bytes_mut = unsafe {
+            slice::from_raw_parts_mut((&mut tmp as *mut _) as *mut u64, 16)
+        };
+        let mut tmp_bytes = unsafe {
+            slice::from_raw_parts((&tmp as *const _) as *const u64, 16)
+        };
+
+        while !acc.is_zero() {
+            let i = (acc.as_ref()[0] & window_mask) as usize;
+            let p2 = unsafe {
+                slice::from_raw_parts((&table[0][i] as *const _) as *const u64,
+                                      16)
+            };
+
+            twisted_edwards_jubjub_add::ext_twisted_ed_add_256(
+                tmp_bytes.try_into().expect("slice needs len of 16"),
+                p2.try_into().expect("slice needs len of 16"),
+                tmp_bytes_mut.try_into().expect("slice needs len of 16")
+            );
 
             acc.shr(window);
             table = &table[1..];
